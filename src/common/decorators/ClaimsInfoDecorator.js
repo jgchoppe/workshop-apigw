460fd6546e0e2e83bfe061fed23a9378727722e4
Jean-Gaël CHOPPE <jean-gael.choppe@epitech.eu>
2023-02-02 15:11:37 +0100

    fix(main): syntax

--- src/common/decorators/ClaimsInfoDecorator.js
+++ src/common/decorators/ClaimsInfoDecorator.js
@@ -1,8 +1,7 @@
  async function ClaimsInfoDecorator(handler, event) {
-  // TODO: Retrieve user id return error if no id has been found
   const userId = event.requestContext.authorizer.claims["sub"]
 
-   return handler(event, userId)
+  return handler(event, userId)
 }
 
 module.exports = {
